3bbc3a4c9ec9b250f5928e222541e8a3f2a2215a
TheDevice <TheDevice@users.noreply.github.com>
2016-10-05 15:36:00 +0200

    Added fflush()-comment to x86-example

--- Templates/Exercises_WS16_17/0_Entwicklungsumgebung_einrichten/HalloWorld_x86/HalloWorld.c
+++ Templates/Exercises_WS16_17/0_Entwicklungsumgebung_einrichten/HalloWorld_x86/HalloWorld.c
@@ -8,6 +8,8 @@ This is a 'Hello-World'-example for x86.
 int main(){
 
 	printf("Hallo Welt\n");
+    
+    //fflush(stdout);      // Use 'fflush()' if you experience problems with console-output while debugging (especially on Windows)
 
 	return 0;
 }
