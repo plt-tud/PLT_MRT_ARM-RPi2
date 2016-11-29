a356e43b8c48aea0e3494369ac004f029901b1fc
TheDevice <TheDevice@users.noreply.github.com>
2016-11-29 13:58:33 +0100

    Edit in compiler-settings for 'mapped-memory'-example

--- Templates/Exercises_WS16_17/Memory_Mapped_Periperals/mem_mapping/Debug/subdir.mk
+++ Templates/Exercises_WS16_17/Memory_Mapped_Periperals/mem_mapping/Debug/subdir.mk
@@ -17,7 +17,7 @@ C_DEPS += \
 %.o: ../%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross GCC Compiler'
-	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	arm-linux-gnueabihf-gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
