c707084121da605cd8d4a2e26e07af7e65adfc37
ichrispa <Chris_Paul.Iatrou@tu-dresden.de>
2018-05-02 14:23:55 +0200

    Beispielcode aus open62541 entfernt.

--- Vorlesungsbeispiele/MRT2_VL-8_Ampel_mit_OPC_UA/src/RemoteAmpel.cpp
+++ Vorlesungsbeispiele/MRT2_VL-8_Ampel_mit_OPC_UA/src/RemoteAmpel.cpp
@@ -36,24 +36,6 @@ void RemoteAmpel::init()
       client = nullptr;
       return;
   }
-
-  /* Read the value attribute of the node. UA_Client_readValueAttribute is a
-    * wrapper for the raw read service available as UA_Client_Service_read. */
-  UA_Variant value; /* Variants can hold scalar values and arrays of any type */
-  UA_Variant_init(&value);
-
-  /* NodeId of the variable holding the current time */
-  const UA_NodeId nodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_SERVER_SERVERSTATUS_CURRENTTIME);
-  retval = UA_Client_readValueAttribute(client, nodeId, &value);
-
-  if(retval == UA_STATUSCODE_GOOD &&
-      UA_Variant_hasScalarType(&value, &UA_TYPES[UA_TYPES_DATETIME])) {
-      UA_DateTime raw_date = *(UA_DateTime *) value.data;
-      UA_DateTimeStruct dts = UA_DateTime_toStruct(raw_date);
-      UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "date is: %u-%u-%u %u:%u:%u.%03u\n",
-                  dts.day, dts.month, dts.year, dts.hour, dts.min, dts.sec, dts.milliSec);
-  }
-
 }
 
 void RemoteAmpel::run()
@@ -64,8 +46,10 @@ void RemoteAmpel::run()
   clientMutex.lock();
   if (client != nullptr)
     if (UA_Client_getState(client) == UA_CLIENTSTATE_DISCONNECTED) {
+      clientMutex.unlock();
       terminate();
       init();
+      clientMutex.lock();
     };
 
   // Renew session once/second to keep our conncetion going
