86eebb0e4ddd8db4258bbda30042ddeba1c2dde6
ichrispa <chris_paul.iatrou@tu-dresden.de>
2019-11-06 12:07:14 +0100

    Lecture example

---
+++ Vorlesungsbeispiele/MRT1_VL-8_SPIBasics/BCM2836_constants.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2018 Leon Urbas <leon.urbas@tu-dresden.de>
+ * Copyright (c) 2019 Chris Iatrou <chris_paul.iatrou@tu-dresden.de>
+ *
+ * Hiermit wird unentgeltlich jeder Person, die eine Kopie der
+ * Software und der zugehörigen Dokumentationen (die "Software")
+ * erhält, die Erlaubnis erteilt, sie uneingeschränkt zu nutzen,
+ * inklusive und ohne Ausnahme mit dem Recht, sie zu verwenden,
+ * zu kopieren, zu verändern, zusammenzufügen, zu veröffentlichen,
+ * zu verbreiten, zu unterlizenzieren und/oder zu verkaufen, und
+ * Personen, denen diese Software überlassen wird, diese Rechte
+ * zu verschaffen, unter den folgenden Bedingungen:
+ *
+ * Der obige Urheberrechtsvermerk und dieser Erlaubnisvermerk
+ * sind in allen Kopien oder Teilkopien der Software beizulegen.
+ *
+ * DIE SOFTWARE WIRD OHNE JEDE AUSDRÜCKLICHE ODER IMPLIZIERTE
+ * GARANTIE BEREITGESTELLT, EINSCHLIEẞLICH DER GARANTIE ZUR
+ * BENUTZUNG FÜR DEN VORGESEHENEN ODER EINEM BESTIMMTEN ZWECK
+ * SOWIE JEGLICHER RECHTSVERLETZUNG, JEDOCH NICHT DARAUF
+ * BESCHRÄNKT. IN KEINEM FALL SIND DIE AUTOREN ODER
+ * COPYRIGHTINHABER FÜR JEGLICHEN SCHADEN ODER SONSTIGE
+ * ANSPRÜCHE HAFTBAR ZU MACHEN, OB INFOLGE DER ERFÜLLUNG EINES
+ * VERTRAGES, EINES DELIKTES ODER ANDERS IM ZUSAMMENHANG MIT
+ * DER SOFTWARE ODER SONSTIGER VERWENDUNG DER SOFTWARE ENTSTANDEN.
+ */
+
+// @ Definitionen
+// Aus Bootloader, Mapping v. physischem Speicher 0x7Exxxxx --> 0x3fxxxxxx
+.equ BCM2836_PERI_BASE,         0x3f000000
+
+// Wir ab den GPIOs (+0x00200000) auf die SPI0 (+x00204000-+x00204014)
+// --> Wir brauchen 0x3f200000 + 0x4
+.equ BCM2836_PERI_MAPSIZE,      0x4100 // Must be >=4k according to manual !
